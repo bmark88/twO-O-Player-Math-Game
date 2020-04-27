6415010264fa665332b1c6a2dc477594fc998558
bmark88 <bradley.mark@hotmail.com>
2020-04-27 17:29:41 +0000

    Add initializer for question class

--- classes/question.rb
+++ classes/question.rb
@@ -1,9 +1,17 @@
 module Questions
   
   class Question
+    attr_accessor :title, :answer
+
+    def initialize(title, answer)
+      @title = title
+      @answer = answer
+    end
   end
 
 end
+
+include Questions
 
 # 3. Question class
 #   - Generates an actual question to be asked
--- main.rb
+++ main.rb
@@ -2,8 +2,14 @@ require_relative "classes/player.rb"
 require_relative "classes/question.rb"
 require_relative "classes/game.rb"
 
-player1 = Player.new('1')
-player2 = Player.new('2')
+player1 = Player.new('1', 3)
+player2 = Player.new('2', 3)
 
-puts player1.player_num
-puts player2.player_num
+puts player1.id
+puts player2.id
+
+q1 = Question.new('What is 1 + 1?', 2)
+puts q1.title
+puts q1.answer
+
+# player1.lives
